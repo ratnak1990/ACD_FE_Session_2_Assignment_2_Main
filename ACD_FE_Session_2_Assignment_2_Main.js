8ec9690664db11817e63e9a4d4d2eaa042e6d7a9
ratnak1990 <ratnak1990@gmail.com>
2016-03-22 12:00:13 +0530

    submitting assignment

---
+++ ACD_FE_Session_2_Assignment_2_Main.js
@@ -0,0 +1,13 @@
+function calculate(a,b){
+    function multiply(a,b)
+    {
+        return (a*b);
+    }
+    ;
+    return multiply(a,b);
+}
+
+var result=calculate(10,20);
+console.log(result);
+alert("Result = "+  result);
+//document.getElementById("result").innerHTML=result;
